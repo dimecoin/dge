6a373d158bcc18b41ca9204f8612b72ad28fb285
dimecoin <dimecoin@gmail.com>
2017-04-17 19:25:43 -0500

    Adding more flexiable build scritp for Turbo C

--- buildtcc.bat
+++ buildtcc.bat
@@ -4,6 +4,8 @@ REM This is the build script for Turbo C.
 REM Needs to run in DOSBox, VM or a real DOS machine.
 REM Turbo C 3.2 download: https://turboc.codeplex.com/
 
+if "%1"=="" goto :HELP
+
 echo ---------------------------------------------------
 
 REM In most cases, you just need to point to your TURBOC directory
@@ -26,7 +28,7 @@ cd %DGE_DIR%\src
 delete *.OBJ
 delete *.EXE
 
-tcc.exe -ml -y -G -O -C -j1 -I%INCLUDE% -L%LIB%  *.c 
+tcc.exe -ml -y -G -O -C -j1 -I%INCLUDE% -L%LIB% %1.c dge.c dge_bmp.c dge_gfx.c
 
 mkdir %DGE_DIR%\build\tc
 copy *.exe %DGE_DIR%\build\tc
@@ -36,4 +38,15 @@ mkdir %DGE_DIR%\build\tc\res\images
 
 copy %DGE_DIR%\res\images\*.bmp %DGE_DIR%\build\tc\res\images
 
+goto :END
+
+:HELP
+echo ##################################################
+echo Usage: buildtcc.bat [program name] (without ext)
+echo 	example: buildtcc.bat test_gfx
+echo ##################################################
+goto :END
+
+
+:END
 
